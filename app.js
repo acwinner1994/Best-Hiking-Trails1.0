bc333f5acb1704073a223c6a1e2e6079ff9a37a9
albert chingis <albertchingis@alberts-MBP.fios-router.home>
2020-01-06 16:13:05 -0500

    little change

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use("/",indexRoutes);
 app.use("/campgrounds",campgroundRoutes);
 app.use("/campgrounds/:id/comments",commentRoutes);
 
-app.listen(3002, function(){
+app.listen(process.env.PORT||3002, function(){
    console.log("The YelpCamp Server Has Started!");
 });
 
