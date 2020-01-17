3357e73479feb622730e937c4a35722ac4965e54
albert chingis <albertchingis@alberts-MBP.fios-router.home>
2020-01-17 17:47:59 -0500

    image upload done

--- modules/campground.js
+++ modules/campground.js
@@ -5,49 +5,55 @@ var mongoose    = require("mongoose");
 // app.use(bodyParser.urlencoded({extended: true}));
 
 
-
-
-
-// SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
-  name: String,
-  image: String,
-  description: String,
-  cost:Number,
-  location:String,
-  createdAt:{type:Date,default:Date.now},
-  author:{
-    id:{
-      type:mongoose.Schema.Types.ObjectId,
-      ref:"user"
-    },
-    username:String
-  },
-  comments:[   //refer to comment id
-    {
-      type:mongoose.Schema.Types.ObjectId,
-      ref:"Comment"
-    }
-  ]
+   name: String,
+   image: String,
+   imageId: String,
+   description: String,
+   cost:Number,
+   location:String,
+   createdAt:{type:Date,default:Date.now},
+   author: {
+      id: {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "User"
+      },
+      username: String
+   },
+   comments: [
+      {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "Comment"
+      }
+   ]
 });
+
+
+// // SCHEMA SETUP
+// var campgroundSchema = new mongoose.Schema({
+//
+//   name: String,
+//   image: String,
+//   description: String,
+//   cost:Number,
+//   location:String,
+//   createdAt:{type:Date,default:Date.now},
+//   author:{
+//     id:{
+//       type:mongoose.Schema.Types.ObjectId,
+//       ref:"user"
+//     },
+//     username:String
+//   },
+//   comments:[   //refer to comment id
+//     {
+//       type:mongoose.Schema.Types.ObjectId,
+//       ref:"Comment"
+//     }
+//   ]
+// });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
 
 //send out a model which is a specific table to operate
 module.exports=Campground;
-
-// Campground.create(
-//      {
-//          name: "Granite Hill",
-//          image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg",
-//          description: "This is a huge granite hill, no bathrooms.  No water. Beautiful granite!"
-//
-//      },
-//      function(err, campground){
-//       if(err){
-//           console.log(err);
-//       } else {
-//           console.log("NEWLY CREATED CAMPGROUND: ");
-//           console.log(campground);
-//       }
-//     });
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,6 +3,29 @@ var router=express.Router();
 var Campground  = require("../modules/campground");
 var middleware = require("../middleware");
 
+var request = require("request");
+var multer = require('multer');
+var storage = multer.diskStorage({
+  filename: function(req, file, callback) {
+    callback(null, Date.now() + file.originalname);
+  }
+});
+var imageFilter = function (req, file, cb) {
+    // accept image files only
+    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
+        return cb(new Error('Only image files are allowed!'), false);
+    }
+    cb(null, true);
+};
+var upload = multer({ storage: storage, fileFilter: imageFilter})
+
+var cloudinary = require('cloudinary');
+cloudinary.config({
+  cloud_name: 'du4bigyf7',
+  api_key: '934946645218272',
+  api_secret: 'iIrdTeqggzc7wJhq-fE1_uxg9G8'
+});
+
 //INDEX - show all campgrounds
 router.get("/",function(req, res){
     // Get all campgrounds from DB
@@ -16,33 +39,59 @@ router.get("/",function(req, res){
 });
 
 //CREATE - add new campground to DB
-router.post("/", function(req, res){
-
-  // get data from form and add to campgrounds array
-  var name = req.body.name;
-  var image = req.body.image;
-  var cost = req.body.cost;
-  var desc = req.body.description;
-  var id=req.user._id;
-  var username=req.user.username;
-  var location=req.body.location;
-
-
-  var newCampground = {name: name, image: image,location:location,cost:cost, description: desc,author:{
-    id:id,
-    username:username
-  },};
-  // Create a new campground and save to DB
-  Campground.create(newCampground, function(err, newlyCreated){
-      if(err){
-          console.log(err);
-      } else {
-          //redirect back to campgrounds page
-          res.redirect("/campgrounds");
+router.post("/", middleware.isLoggedIn, upload.single('image'), function(req, res) {
+    cloudinary.v2.uploader.upload(req.file.path, function(err, result) {
+      if(err) {
+        req.flash('error', err.message);
+        return res.redirect('back');
       }
-  });
-
+      // add cloudinary url for the image to the campground object under image property
+      req.body.campground.image = result.secure_url;
+      // add image's public_id to campground object
+      req.body.campground.imageId = result.public_id;
+      // add author to campground
+      req.body.campground.author = {
+        id: req.user._id,
+        username: req.user.username
+      }
+      Campground.create(req.body.campground, function(err, campground) {
+        if (err) {
+          req.flash('error', err.message);
+          return res.redirect('back');
+        }
+        res.redirect('/campgrounds/' + campground.id);
+      });
+    });
 });
+
+// //CREATE - add new campground to DB
+// router.post("/", function(req, res){
+//
+//   // get data from form and add to campgrounds array
+//   var name = req.body.name;
+//   var image = req.body.image;
+//   var cost = req.body.cost;
+//   var desc = req.body.description;
+//   var id=req.user._id;
+//   var username=req.user.username;
+//   var location=req.body.location;
+//
+//
+//   var newCampground = {name: name, image: image,location:location,cost:cost, description: desc,author:{
+//     id:id,
+//     username:username
+//   },};
+//   // Create a new campground and save to DB
+//   Campground.create(newCampground, function(err, newlyCreated){
+//       if(err){
+//           console.log(err);
+//       } else {
+//           //redirect back to campgrounds page
+//           res.redirect("/campgrounds");
+//       }
+//   });
+//
+// });
 
 
 
